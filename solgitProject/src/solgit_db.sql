d6a4341460b2f398e12ceb4698b10a9e711f6547
이보람 <fruitfuls2@naver.com>
2020-04-30 19:38:38 +0900

    sql 수정

--- solgitProject/src/solgit_db.sql
+++ solgitProject/src/solgit_db.sql
@@ -39,3 +39,57 @@ VALUES
     'N', 
     1);
 select * from BOARD;
+
+
+
+
+
+DROP TABLE member CASCADE CONSTRAINT;
+DROP SEQUENCE member_Seq;
+
+CREATE SEQUENCE member_seq
+START WITH 1
+INCREMENT BY 1;
+
+CREATE TABLE member
+(
+    member_Seq        NUMBER           NOT NULL, 
+    member_Email         VARCHAR2(20)     NOT NULL, 
+    member_PW         VARCHAR2(20)    NOT NULL, 
+    member_Phone      NUMBER     		NOT NULL, 
+    member_name    VARCHAR(1000)    NOT NULL, 
+    member_Date       DATE             NOT NULL, 
+    member_Manager	    	NUMBER		    	NULL,
+    CONSTRAINT MEMBER_PK1 PRIMARY KEY (member_Seq)
+);
+
+
+
+INSERT INTO member
+VALUES
+    (
+    member_Seq.nextval,
+    'Member_email 1', 
+    'Member_pw 1', 
+    010-0000-0000, 
+    'Member_name 1', 
+    '05/03/23',
+    1
+    );
+
+INSERT INTO member
+VALUES
+    (
+    member_Seq.nextval,
+    'admin@naver.com', 
+    '1', 
+    010-0000-0000, 
+    'Member_name 1', 
+    '05/03/23',
+    1
+    );
+    
+select * from member;
+
+
+select * from MEMBER where MEMBER_EMAIL='admin@naver.com' and MEMBER_PW='1';
